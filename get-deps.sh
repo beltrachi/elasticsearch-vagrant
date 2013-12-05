1fbc1c443fdb90b3b20b64681a222dffcdd7bfcc
Jordi Beltran <beltrachi@gmail.com>
2013-12-05 18:43:07 +0100

    Remove git-deps.sh as useless or not documented

--- get-deps.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p pkg
-$(cd pkg && wget https://s3.amazonaws.com/tarn-repo/libjzmq_2.1.7_amd64.deb)
-$(cd pkg && wget https://s3.amazonaws.com/tarn-repo/libzmq1_2.1.7_amd64.deb)
-$(cd pkg && wget https://s3.amazonaws.com/tarn-repo/storm_0.8.1_all.deb)
-
-# cache these too.
-$(cd pkg && wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.1.tar.gz)
-$(cd pkg && wget http://s3.amazonaws.com/downloads.basho.com/riak/CURRENT/debian/6/riak_1.2.1-1_amd64.deb)
-$(cd pkg && wget http://redis.googlecode.com/files/redis-2.4.13.tar.gz)
